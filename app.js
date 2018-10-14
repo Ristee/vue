166c506977cca1eb92821c800e17db03160e9717
ristee <rristee@gmail.com>
2018-10-14 23:39:21 +0500

    https://youtu.be/_F8zxL9KEnU?t=780 lesson5 middle

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-/*function car(name, model, owner, phone, image) {
-    return {
-        name, model,owner, phone, image
-    }
-}*/
 
-const car  = (name, model, owner, phone, image) =>({name, model, owner, phone, image});
+
+const car  = (name, model, owner, year, phone, image) =>({name, model, owner, year, phone, image});
 
 const cars = [
     car('Ford', 'Focus', 'Max', 2016, '+79293456789', 'images/focus.jpg'),
-    car('Ford', 'Mondeo', 'Kate', 2013, '+7922312421', 'images/modeo.jpg'),
+    car('Ford', 'Mondeo', 'Kate', 2013, '+7922312421', 'images/mondeo.jpg'),
     car('Porsche', 'Panamera', 'Ivan', 2017, '+79293456789', 'images/panamera.jpg')    
-]
+];
 
 new Vue({
     el: '#app',
     data: {
-        cars: cars
+        cars: cars,
+        car: cars[0],
+        selectedIndex: 0,
+        phoneVisibility: false,
+        search: ''
+    },
+    methods: {
+        selectCar: function(index) {
+            this.car = cars[index];
+            this.selectedIndex = index;
+        },
+        showPhone: function() {
+            this.hidePhone = false;
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide phone' : 'Show phone';
+        },
+        filteredCars() {
+            var self = this;
+            const filtered = this.cars.filter(function(car) {
+                return car.name.toLowerCase().indexOf(self.search.toLowerCase()) > -1;
+            });
+            return filtered;
+        }
     }
 });
